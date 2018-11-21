25333f75e1ad36bd4182327ccb4a457b196a9b6b
appkr <juwonkim@me.com>
2018-11-21 15:44:15 +0900

    ch03 스프링 DI - 조립기

---
+++ src/main/java/spring/MemberDao.java
@@ -0,0 +1,21 @@
+package spring;
+
+import java.util.HashMap;
+
+public class MemberDao {
+    private static long nextId = 0;
+    private HashMap<String, Member> map = new HashMap<>();
+
+    public Member selectByEmail(String email) {
+        return map.get(email);
+    }
+
+    public void insert(Member member) {
+        member.setId(++nextId);
+        map.put(member.getEmail(), member);
+    }
+
+    public void update(Member member) {
+        map.put(member.getEmail(), member);
+    }
+}
