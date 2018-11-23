8beba6806408c517f8923abd55057214784b5c6f
appkr <juwonkim@me.com>
2018-11-24 00:08:31 +0900

    ch07 AOP 프로그래밍 - ExeTimeAspect 추가

---
+++ src/main/java/main/MainAspect.java
@@ -0,0 +1,17 @@
+package main;
+
+import aop.Calculator;
+import config.AppCtx;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainAspect {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx
+            = new AnnotationConfigApplicationContext(AppCtx.class);
+        Calculator cal = ctx.getBean("calculator", Calculator.class);
+        long fiveFact = cal.factorial(5);
+        System.out.println("cal.factorial(5) = " + fiveFact);
+        System.out.println(cal.getClass().getName());
+        ctx.close();
+    }
+}
