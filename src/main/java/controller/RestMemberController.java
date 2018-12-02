312426c2bd5611f62e2a628238f1ae7fbad6bad0
appkr <juwonkim@me.com>
2018-12-02 16:16:26 +0900

    ch16 JSON 응답과 요청 처리 - ResponseEntity를 이용한 응답 처리

--- src/main/java/controller/RestMemberController.java
+++ src/main/java/controller/RestMemberController.java
@@ -1,13 +1,12 @@
 package controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import spring.Member;
-import spring.MemberDao;
-import spring.MemberRegisterService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import spring.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,14 +21,25 @@ public class RestMemberController {
     }
 
     @GetMapping("/api/members/{id}")
-    public Member member(@PathVariable Long id, HttpServletResponse response) throws IOException {
+    public ResponseEntity<Object> member(@PathVariable Long id) throws IOException {
         Member member = memberDao.selectById(id);
         if (member == null) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return null;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse("no member"));
         }
 
-        return member;
+        return ResponseEntity.status(HttpStatus.OK).body(member);
+    }
+
+    @PostMapping("/api/members")
+    public void newMember(@RequestBody @Valid RegisterRequest regReq, HttpServletResponse response) throws IOException {
+        try {
+            Long newMemberId = registerService.regist(regReq);
+            response.setHeader("Location", "/api/members/" + newMemberId);
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        } catch (DuplicateMemberException e) {
+            response.sendError(HttpServletResponse.SC_CONFLICT);
+        }
     }
 
     public void setMemberDao(MemberDao memberDao) {
